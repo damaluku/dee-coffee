dee9c28f216a588071c1d8e5d08d9c61dac9176d
damaluku <damaluku@gmail.com>
2023-06-26 13:45:10 +0100

    createCoffeeStore api created

---
+++ lib/airtable.ts
@@ -0,0 +1,19 @@
+const Airtable = require("airtable");
+
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_TOKEN }).base(
+  process.env.AIRTABLE_BASE_ID
+);
+
+const table = base("coffee-stores");
+
+const getMinifiedRecord = (record: any) => {
+  return {
+    ...record.fields,
+  };
+};
+
+const getMinifiedRecords = (records: any) => {
+  return records.map((record: any) => getMinifiedRecord(record));
+};
+
+export { getMinifiedRecords, table };
--- pages/api/createCoffeeStore.ts
+++ pages/api/createCoffeeStore.ts
@@ -1,34 +1,76 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { fetchCoffeeStores } from "@/lib/coffee-stores";
+import { getMinifiedRecords, table } from "@/lib/airtable";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
+/* type Data = {
   table: any;
   message: string;
-};
-
-const Airtable = require("airtable");
-
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_TOKEN }).base(
-  process.env.AIRTABLE_BASE_ID
-);
-
-const table = base("coffee-stores");
-
-console.log(table);
+}; */
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
   if (req.method === "POST") {
-    res.status(200).json({
-      table,
-      message: "correct request method",
-    });
-  } else
-    res.status(500).json({
-      table: null,
-      message: "Not correct request method",
-    });
+    const {
+      id,
+      name,
+      address,
+      neighbourhood,
+      voting,
+      imgUrl,
+      cross_street,
+      locality,
+      formatted_address,
+      distance,
+    } = req.body;
+
+    try {
+      if (id) {
+        const findCoffeeStoreRecord = await table
+          .select({
+            filterByFormula: `id=${id}`,
+          })
+          .firstPage();
+
+        if (findCoffeeStoreRecord.length !== 0) {
+          const records = getMinifiedRecords(findCoffeeStoreRecord);
+
+          res.status(200).json(records);
+        } else {
+          if (name) {
+            const createRecords = await table.create([
+              {
+                fields: {
+                  id,
+                  name,
+                  address,
+                  neighbourhood,
+                  voting,
+                  imgUrl,
+                  cross_street,
+                  locality,
+                  formatted_address,
+                  distance,
+                },
+              },
+            ]);
+
+            const records = getMinifiedRecords(createRecords);
+
+            res.json(records);
+          } else {
+            res.status(400).json({ message: "id or name is missing!" });
+          }
+        }
+      } else {
+        res.status(400).json({ message: "id is missing!" });
+      }
+    } catch (error) {
+      console.error("Error finding or creating a store", error);
+      res
+        .status(500)
+        .json({ message: "Error finding or creating a store", error });
+    }
+  }
 }
